55c4609d89a3c54af6e37a25b1f244d13f980af7
winterrr03 <dong.huynhquang21503@gmail.com>
2024-07-21 23:25:20 +0700

    feat(media): streaming video

--- src/controllers/medias.controllers.ts
+++ src/controllers/medias.controllers.ts
@@ -3,6 +3,8 @@ import path from 'path'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from '~/constants/dir'
 import { USERS_MESSAGES } from '~/constants/messages'
 import mediasService from '~/services/medias.services'
+import fs from 'fs'
+import HTTP_STATUS from '~/constants/httpStatus'
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
   const url = await mediasService.uploadImage(req)
@@ -29,11 +31,28 @@ export const serveImageController = (req: Request, res: Response, next: NextFunc
   })
 }
 
-export const serveVideoController = (req: Request, res: Response, next: NextFunction) => {
+export const serveVideoStreamController = async (req: Request, res: Response, next: NextFunction) => {
+  const range = req.headers.range
+  if (!range) {
+    return res.status(HTTP_STATUS.BAD_REQUEST).send('Requires Range header')
+  }
   const { name } = req.params
-  return res.sendFile(path.resolve(UPLOAD_VIDEO_DIR, name), (err) => {
-    if (err) {
-      res.status((err as any).status).send('Not found')
-    }
-  })
+  const videoPath = path.resolve(UPLOAD_VIDEO_DIR, name)
+  const videoSize = fs.statSync(videoPath).size
+  const chunkSize = 3 * 10 ** 6
+  const start = Number(range.replace(/\D/g, ''))
+  const end = Math.min(start + chunkSize, videoSize - 1)
+
+  const contentLength = end - start + 1
+
+  const contentType = (await import('mime')).default.getType(videoPath) || 'video/*'
+  const headers = {
+    'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': contentLength,
+    'Content-Type': contentType
+  }
+  res.writeHead(HTTP_STATUS.PARTIAL_CONTENT, headers)
+  const videoSteams = fs.createReadStream(videoPath, { start, end })
+  videoSteams.pipe(res)
 }
