cc5e11bd4bd0cfcd5bc41296b214f1fff0cb1502
winterrr03 <dong.huynhquang21503@gmail.com>
2024-07-21 17:24:58 +0700

    feat(media): filter upload image

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,3 +1,4 @@
+import { Request } from 'express'
 import fs from 'fs'
 import path from 'path'
 
@@ -8,4 +9,33 @@ export const initFolder = () => {
       recursive: true
     })
   }
+}
+
+export const handleUploadSingleImage = async (req: Request) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024, // KB
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
 }
